314bd5e7b617f995baa400383332e00d9eaae1a4
Alberto Basaglia <alberto.basaglia17@gmail.com>
2025-01-13 09:40:31 +0100

    remove unused test

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,3 @@
 from torch.utils.data import Dataset
 
-from segmentationsuim.data import MyDataset
-
-
-def test_my_dataset():
-    """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
-    assert isinstance(dataset, Dataset)
+# from segmentationsuim.data import download_dataset
