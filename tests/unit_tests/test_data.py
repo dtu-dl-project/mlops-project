3e287e961371c55e3aa8b53bbdd426fd85d4130d
lovomanuel <163330676+lovomanuel@users.noreply.github.com>
2025-01-21 08:18:06 +0100

    add unit tests

--- tests/unit_tests/test_data.py
+++ tests/unit_tests/test_data.py
@@ -1,5 +1,9 @@
 from segmentationsuim.data import download_dataset
 from segmentationsuim.data import grayscale_to_class
+from segmentationsuim.data import SUIMDatasetRaw
+from segmentationsuim.data import SUIMDatasetProcessed
+from segmentationsuim.data import save_processed_dataset
+from torchvision import transforms
 from PIL import Image
 import numpy as np
 
@@ -16,3 +20,27 @@ def test_grayscale_to_class():
     class_map = grayscale_to_class(grayscale_image)
     expected_class_map = np.array([[0, 1, 2], [3, 4, 5]], dtype=np.uint8)
     assert np.array_equal(class_map, expected_class_map), "Grayscale to class mapping failed."
+
+
+def test_Dataset_class():
+    path = "tests/test_data/raw"
+    dataset = SUIMDatasetRaw(path)
+    image, mask = dataset[0]
+    assert dataset is not None
+    assert len(dataset) == 3
+    assert image.dim() == 3
+    assert mask.dim() == 2
+
+
+def test_download_and_load_processed():
+    path_raw = "tests/test_data/raw"
+    path_processed = "tests/test_data/processed"
+    image_transform = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
+    mask_transform = transforms.Compose([transforms.Resize((256, 256), interpolation=Image.NEAREST)])
+    dataset = SUIMDatasetRaw(path_raw, image_transform=image_transform, mask_transform=mask_transform)
+    save_processed_dataset(dataset, path_processed)
+    dataset1 = SUIMDatasetProcessed(path_processed)
+    image, mask = dataset[0]
+    assert dataset1 is not None
+    assert image.dim() == 3
+    assert mask.dim() == 2
